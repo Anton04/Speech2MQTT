288453c165b67b128b6ebd2659e8e25a2e55b4d5
gustafsson <anton.gustafsson@tii.se>
2014-04-23 23:41:04 +0200

    bugfix

--- Speech2MQTT.py
+++ Speech2MQTT.py
@@ -1,8 +1,13 @@
 #!/usr/bin/python
 
-
+from json import loads
+from urllib2 import Request, urlopen
+from wave import open as open_audio
+from pyaudio import PyAudio, paInt16
 from pygsr import Pygsr
 import audioop
+import urllib2
+
 
 class Speech2MQTT(Pygsr):
 
@@ -13,28 +18,45 @@ class Speech2MQTT(Pygsr):
     return 
     
     
-  def listen(self, level = 40,timeout = 1,language = "sv_SE", device_i=None):
+  def listen(self, level = 1000,timeout = 1,language = "sv_SE", device_i=None):
     while(True):
-      print self.listen_once(self, level,timeout,language, device_i)
-  
+      print self.listen_once(level,timeout,language, device_i)
     
-  def listen_once(self, level = 40,timeout = 1,language = "sv_SE", device_i=None):
+    print "Next loop!"
+  
+    return 
+    
+  def listen(self, level = 1000,timeout = 1,language = "sv_SE",loop=1, device_i=None):
     audio = PyAudio()
-    print audio.get_device_info_by_index(1)
+    #print audio.get_device_info_by_index(1)
     stream = audio.open(input_device_index=device_i,output_device_index=device_i,format=self.format, channels=self.channel,
                             rate=self.rate, input=True,
                             frames_per_buffer=self.chunk)
-    print "REC: "
+
+   
+    print "Start listening... "
     frames = []
+    data = ""
+    olddata = ""
+    self.count_silence = 0
+    self.active = False
+
     while(True):  #for i in range(0, self.rate / self.chunk * time):
       data = stream.read(self.chunk)
       rms = audioop.rms(data, 2)
+
+      #print str(rms) + '\r'
             
       #There is some noise start recording
-      if rms > level and self.active == False:
-        self.active = True
-        self.count_silence = 0
-                
+      if rms > level:
+	self.count_silence = 0
+        
+	if self.active == False:
+	  print "Recording..."
+          self.active = True
+          self.count_silence = 0
+	  frames.append(olddata)
+
       if self.active:       
         frames.append(data)
               
@@ -44,28 +66,48 @@ class Speech2MQTT(Pygsr):
       #If we have enough silence send for processing  
       if self.count_silence > (self.rate / self.chunk * timeout):
         self.active = False
+	print "Processing..."
         break;
-              
-      stream.stop_stream()
-      print "END"
-      stream.close()
-      audio.terminate()
-      write_frames = open_audio(self.file, 'wb')
-      write_frames.setnchannels(self.channel)
-      write_frames.setsampwidth(audio.get_sample_size(self.format))
-      write_frames.setframerate(self.rate)
-      write_frames.writeframes(''.join(frames))
-      write_frames.close()
-      self.convert()
-      
-      phrase, complete_response = self.speech_to_text(language) # select the language
+    
+      olddata = data      
+ 
+    stream.stop_stream()
+    #print "END"
+    stream.close()
+    audio.terminate()
+         
+    write_frames = open_audio(self.file, 'wb')
+    write_frames.setnchannels(self.channel)
+    write_frames.setsampwidth(audio.get_sample_size(self.format))
+    write_frames.setframerate(self.rate)
+    write_frames.writeframes(''.join(frames))
+    write_frames.close()
+    self.convert()
 
-      return phrase
+    try:      
+    	phrase, complete_response = self.speech_to_text(language) # select the language
+    except:
+	phrase = ""
+#    except IndexError:
+#	try:
+# 	  phrase, complete_response = self.speech_to_text(language)
+#	except:
+#	  phrase = ""
+
+    #print phrase
+
+#    stream.stop_stream()
+    #print "END"
+#    stream.close()
+#    audio.terminate()
+
+    return phrase
+
 
 
 if __name__ == '__main__':
   listner = Speech2MQTT()
-  print listner.listen_once()
+  print listner.listen()
   
   
 
